c3761b54acfe01755587f2e6b011c6964d989f54
Tim <jasper7466@yandex.ru>
2022-12-22 23:33:35 +0500

    Fix. Отключен рендеринг виджета на не проинициализированной карточке

--- src/callbacks/render.ts
+++ src/callbacks/render.ts
@@ -3,7 +3,13 @@ import { renderInPanel } from '../utils/renderInPanel.js';
 
 const render = function (this: IWidget) {
   console.log('render');
-  renderInPanel.call(this);
+
+  if (
+    window.AMOCRM.data.current_card &&
+    window.AMOCRM.data.current_card.id !== 0
+  ) {
+    renderInPanel.call(this);
+  }
 };
 
 export { render };
